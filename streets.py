da78b60558f65ed4e605c281b553cf2405707889
ijoo2 <inhajoo9@gmail.com>
2016-02-27 23:30:39 -0600

    stuff

--- streets.py
+++ streets.py
@@ -1,8 +1,12 @@
 from imposm.parser import OSMParser
+from config import config
+from osmdata import Graph
 
 class Streets(object):
     def __init__(self):
-        pass
+        print "Reading OpenStreetMap data"
+        data = Graph(config['osm_data'])
 
-if __name__ == "__main__"
+
+if __name__ == "__main__":
     Streets()
